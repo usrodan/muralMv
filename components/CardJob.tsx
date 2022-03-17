b8d12565e574332e1764867928cd5315cd66d63b
Danilo Facco <faccodanilo@gmail.com>
2022-03-17 18:21:29 +0000

    Update CardJob.tsx

--- components/CardJob.tsx
+++ components/CardJob.tsx
@@ -1,6 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import Image from "next/image"
-import {format} from 'date-fns'
+import { format } from 'date-fns'
 interface CardJobProps {
   image: string;
   title: string;
@@ -8,19 +8,19 @@ interface CardJobProps {
   date: string;
   type: string;
   id: string;
-  
+
 }
 
-const CardJob: React.FC<CardJobProps> = ({ image, title, city, date, type,id }) => {
-const formatedData = format(new Date(date || '2022-03-03T10:00:38.765Z' ),"dd/MM/yyy")
+const CardJob: React.FC<CardJobProps> = ({ image, title, city, date, type, id }) => {
+  const formatedData = format(new Date(date || '2022-03-03T10:00:38.765Z'), "dd/MM/yyy")
   return (
     <a href={`/${id}`} className="flex  h-72 border border-gray-200 lg:h-80 xl:h-96 font-bold flex-col  rounded-lg bg-white">
-      <Image className="rounded-t-lg object-cover" alt={title} width={256} height={310} src={image || "http://placehold.jp/ffffff/ffffff/256x310.png?text=%20" } />
+      <Image className="rounded-t-lg object-cover" alt={title} width={256} height={310} src={image || "http://placehold.jp/ffffff/ffffff/256x310.png?text=%20"} />
       <div className="w-full flex text-sm flex-col p-2">
         <span className="text-primary uppercase">{title}</span>
         <div className="flex uppercase justify-between w-full">
-          <span>{city}<br/> 
-          <span className="text-xs text-gray-500">{formatedData}</span></span>
+          <span>{city}<br />
+            <span className="text-xs text-gray-500">{formatedData}</span></span>
           <span >{type}</span>
         </div>
       </div>
