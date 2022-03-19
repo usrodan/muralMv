906b6ac1dd4277f23b88426bbcfe8bd6c3ff995f
Danilo Facco <faccodanilo@gmail.com>
2022-03-19 19:12:10 +0000

    Update [id].tsx

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -25,65 +25,72 @@ const IndexPage = ({ buildTimestamp, mural }) => {
               <Sidebar />
             </div>
             <div className="col-span-12 md:col-span-9 ">
-              {mural ? 
-              <section className='flex flex-col bg-white rounded-lg border border-gray-300'>
-                <div className='flex flex-col p-5'>
-                  <strong className="text-blue-500 uppercase text-3xl ">{mural.cargo}</strong>
-                  <span className="text-base uppercase font-semibold">{mural.cidade} • {formatedData}</span>
-                  <span className="text-base uppercase font-semibold">{mural.tipo}</span>
+              {mural ?
+                <section className='flex flex-col bg-white rounded-lg border border-gray-300'>
+                  <div className='flex flex-col p-5'>
+                    <strong className="text-blue-500 uppercase text-3xl ">{mural.cargo}</strong>
+                    <span className="text-base uppercase font-semibold">{mural.cidade} • {formatedData}</span>
+                    <span className="text-base uppercase font-semibold">{mural.tipo}</span>
                   </div>
-              <div className="w-full grid sm:grid-cols-2 p-5 pt-0 gap-5 "> 
-                  <Image className="rounded-lg  " alt={mural.cargo} width={mural.imgW} height={mural.imgH} src={mural.image || "https://placehold.jp/ffffff/ffffff/256x310.png?text=%20"} />
-  
-                <div className="flex flex-col justify-between  ">
-                  <section className="flex flex-col gap-3">
+                  <div className="w-full grid sm:grid-cols-2 p-5 pt-0 gap-5 ">
+
+                    {mural.imgW < mural.imgH && "Vertical"}
+                    <Image className="rounded-lg  " alt={mural.cargo} width={mural.imgW} height={mural.imgH} src={mural.image || "https://placehold.jp/ffffff/ffffff/256x310.png?text=%20"} />
+                    {mural.imgW > mural.imgH && "Horizontal"}
+                    {mural.imgW == mural.imgH && "Quadrada"}
+                    
+                    <div className="flex flex-col justify-between  ">
+                      <section className="flex flex-col gap-3">
 
 
-                    <div className="flex  hover:opacity-60 cursor-pointer gap-2 border p-2 border-gray-800 rounded-lg text-center justify-center w-full">
-                      <ShareIos size={24} />
-                      <span className="font-semibold text-base">COMPARTILHAR ESSA VAGA</span>
+                        <div className="flex  hover:opacity-60 cursor-pointer gap-2 border p-2 border-gray-800 rounded-lg text-center justify-center w-full">
+                          <ShareIos size={24} />
+                          <span className="font-semibold text-base">COMPARTILHAR ESSA VAGA</span>
+                        </div>
+
+                        {mural.imgW < mural.imgH && "Vertical"}
+                        {mural.imgW > mural.imgH && "Horizontal"}
+                        {mural.imgW == mural.imgH && "Quadrada"}
+
+                      </section>
+
+                      <section className="flex flex-col gap-5">
+                        <div className="w-full flex items-center">
+                          <div className="flex flex-1 mr-2 h-0.5 border-t border-dashed border-gray-800" />
+                          <span style={{ fontFamily: 'Pacifico' }} className="text-center text-2xl ">quer mais vagas?</span>
+                          <div className="flex flex-1 ml-2 h-0.5  border-t border-dashed border-gray-800" />
+                        </div>
+
+                        <div className="bg-teal-100 border-t-4 flex p-3 text-teal-900 gap-2 border-teal-500">
+                          <div className="w-8 h-8"><InfoCircle size={32} /></div>
+                          <p className="flex flex-col">
+                            <span className="text-sm font-bold">Fique por dentro de outras vagas de emprego</span>
+                            <span className="text-xs">Entre em um de nossos grupos e saia na frente!</span>
+                          </p>
+
+                        </div>
+
+                        <div className="grid md:grid-cols-2 gap-4 w-full text-white text-xs font-semibold text-center">
+                          <div className="flex bg-whatsapp hover:bg-opacity-60 cursor-pointer items-center rounded-lg p-2 w-full gap-2 ">
+                            <Whatsapp size={20} />
+                            GRUPO NO WHATSAPP
+                          </div>
+
+                          <div className="flex bg-telegram  hover:bg-opacity-60 cursor-pointer items-center rounded-lg p-2 w-full gap-2">
+                            <Telegram size={20} />
+                            CANAL NO TELEGRAM
+                          </div>
+                        </div>
+
+                        <div className="text-red-500  hover:opacity-60 cursor-pointer flex gap-2 font-semibold">
+                          <ExclamationOctagon size={20} />
+                          <span>DENUNCIAR VAGA</span>
+                        </div>
+
+                      </section>
                     </div>
-
-                    {mural.imgW > mural.imgH ? "Horizontal" : "Vertical"}
-                  </section>
-
-                  <section className="flex flex-col gap-5">
-                    <div className="w-full flex items-center">
-                      <div className="flex flex-1 mr-2 h-0.5 border-t border-dashed border-gray-800" />
-                      <span style={{ fontFamily: 'Pacifico' }} className="text-center text-2xl ">quer mais vagas?</span>
-                      <div className="flex flex-1 ml-2 h-0.5  border-t border-dashed border-gray-800" />
-                    </div>
-
-                    <div className="bg-teal-100 border-t-4 flex p-3 text-teal-900 gap-2 border-teal-500">
-                      <div className="w-8 h-8"><InfoCircle size={32} /></div>
-                      <p className="flex flex-col">
-                        <span className="text-sm font-bold">Fique por dentro de outras vagas de emprego</span>
-                        <span className="text-xs">Entre em um de nossos grupos e saia na frente!</span>
-                      </p>
-
-                    </div>
-
-                    <div className="grid md:grid-cols-2 gap-4 w-full text-white text-xs font-semibold text-center">
-                      <div className="flex bg-whatsapp hover:bg-opacity-60 cursor-pointer items-center rounded-lg p-2 w-full gap-2 ">
-                        <Whatsapp size={20} />
-                        GRUPO NO WHATSAPP
-                      </div>
-
-                      <div className="flex bg-telegram  hover:bg-opacity-60 cursor-pointer items-center rounded-lg p-2 w-full gap-2">
-                        <Telegram size={20} />
-                        CANAL NO TELEGRAM
-                      </div>
-                    </div>
-
-                    <div className="text-red-500  hover:opacity-60 cursor-pointer flex gap-2 font-semibold">
-                      <ExclamationOctagon size={20} />
-                      <span>DENUNCIAR VAGA</span>
-                    </div>
-
-                  </section>
-                </div>
-              </div>
-              </section>
+                  </div>
+                </section>
                 :
                 <div className="w-full  ">
                   <p>Nenhuma vaga encontrada.</p>
