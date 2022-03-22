88d69b58eb12d143eaba59a2761a0e3863e310f4
Danilo Facco <faccodanilo@gmail.com>
2022-03-22 10:33:24 +0000

    Added Google tag Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,37 @@
 import "@/css/index.css";
 import Head from "next/head";
 import Layout from "@/components/Layout";
-import 'react-toastify/dist/ReactToastify.css'; 
+import 'react-toastify/dist/ReactToastify.css';
 import { ApolloProvider } from "@apollo/client";
 import client from "@/utils/apollo";
-
+import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
       </Head>
       <ApolloProvider client={client}>
+
+        <Script id="google-ads" strategy="afterInteractive" nonce='8JViWmwJBu' >
+          {`
+            window.addEventListener("load",function(){
+              const script = document.createElement("script");
+              script.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6873518969054710";
+              document.body.appendChild(script);
+            }) 
+          `}
+        </Script>
+
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-7MKJYHFTPR" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-7MKJYHFTPR');
+            `}
+        </Script>
         <Component {...pageProps} />
       </ApolloProvider>
     </Layout>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+
 import { ToastContainer } from 'react-toastify';
 
 class MyDocument extends Document {
@@ -17,17 +18,6 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Pacifico&display=swap" rel="stylesheet"></link>
-          {/*<GoogleAnalytics id="UA-44242617-11"/> */}
-
-          
-          <script nonce='8JViWmwJBu' dangerouslySetInnerHTML={{
-            __html: `
-            window.addEventListener("load",function(){
-              const script = document.createElement("script");
-              script.src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6873518969054710";
-              document.body.appendChild(script);
-            }) `}} /> 
-          
         </Head>
         <body>
           <Main />
