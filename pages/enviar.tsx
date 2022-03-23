dd3e8106c9851ccbf3f17397151122a374c244d4
Danilo Facco <faccodanilo@gmail.com>
2022-03-23 15:41:56 +0000

    Update enviar.tsx

--- pages/enviar.tsx
+++ pages/enviar.tsx
@@ -197,7 +197,7 @@ const EnviarPage = () => {
               <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
                 <div>
                   <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-blue-100">
-                    <Info className="h-6 w-6 text-blue-500" aria-hidden="true" />
+                    <Info size={30} className="h-8 w-8 text-blue-500" aria-hidden="true" />
                   </div>
                   <div className="mt-3 text-center sm:mt-5">
                     <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
