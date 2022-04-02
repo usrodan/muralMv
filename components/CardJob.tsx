c4d36c171bdda65cf8e9cb327537cb82f6630f4b
Danilo Facco <faccodanilo@gmail.com>
2022-04-02 16:45:15 +0100

    Update CardJob.tsx

--- components/CardJob.tsx
+++ components/CardJob.tsx
@@ -30,15 +30,15 @@ const CardJob: React.FC<CardJobProps> = ({ image, title, city, date, type, color
       </div>
     </a>
     {/* MOBILE */}
-    <a href={`/${id}-${slugify(title)}`} className="grid grid-cols-3 md:hidden  hover:-mt-2 focu:-mt-2  transition-all duration-500 ease-in-out	 border border-gray-200     font-bold  rounded-lg bg-white">
+    <a href={`/${id}-${slugify(title)}`} className="grid grid-cols-3 md:hidden focus:border-gray-300  transition-all duration-500 ease-in-out	 border border-gray-200     font-bold  rounded-lg bg-white">
       <Image className="rounded-l-lg object-cover hover:opacity-80  " alt={title} width={200} height={200} src={image || "http://placehold.jp/ffffff/ffffff/256x310.png?text=%20"} />
-      <div className="w-full justify-between flex text-sm flex-col col-span-2 p-2">
+      <div className="w-full justify-between flex text-sm flex-col col-span-2 p-3">
         <span className="text-primary uppercase">{title}</span>
         <div className="flex uppercase justify-between items-end w-full">
           <span className="text-xs">{city.replace("1. ", "").replace("2. ", "")}<br />
             <span className="text-xs text-gray-500">{formatedData}</span>
           </span>
-          <div><span className={`inline-flex   uppercase items-center px-2 py-1 rounded-full font-medium bg-${color}-100 text-${color}-800`}>{type}</span></div>
+          <div><span className={`inline-flex text-xs uppercase items-center px-3 py-1 rounded-lg  bg-${color}-100 text-${color}-800`}>{type}</span></div>
         </div>
       </div>
     </a>
