be8b4d5eabc688fe7591871366fe965a8ca6eecd
Danilo Facco <faccodanilo@gmail.com>
2022-04-02 17:58:01 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -183,13 +183,24 @@ const IndexPage = ({ buildTimestamp }) => {
                         </Script>
                       </div>}
 
-                      {index == 6 && <div className='max-h-[382px] overflow-hidden border border-gray-200 p-2 rounded-lg bg-white'   >
-                        <div className='max-h-[350px] overflow-hidden'>
+                      {index == 6 && <div className='border border-gray-200 p-2 rounded-lg bg-white'   >
+                        <div className='hidden md:flex '> 
+
+                            <ins className="adsbygoogle"
+                              style={{ display: "inline-block", width: "350px", height:"265px"}}
+                              data-ad-client="ca-pub-6873518969054710"
+                              data-ad-slot="9849639513" 
+                              data-full-width-responsive="true"></ins>
+                            <Script id={`MURAL-JOB-350*265-${index}`}>
+                              {`(adsbygoogle = window.adsbygoogle || []).push({ });`}
+                            </Script>
+                        </div>
+
+                        <div className='md:hidden overflow-hidden'>
                           <ins className="adsbygoogle"
-                            style={{ display: "flex", maxHeight: "382px !important" }}
+                            style={{ display: "flex"  }}
                             data-ad-client="ca-pub-6873518969054710"
-                            data-ad-slot="5850319795"
-                            data-ad-format="auto"
+                            data-ad-slot="5850319795" 
                             data-full-width-responsive="true"></ins>
                           <Script id={`MURAL-CARDJOBS-${index}`}>
                             {`(adsbygoogle = window.adsbygoogle || []).push({ });`}
